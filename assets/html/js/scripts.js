9acdb6689a7642fd78c0270056bef145628fbdfa
Caleb <caleb.tutty@nzherald.co.nz>
2015-03-26 16:24:22 +1300

    Initial commit

---
+++ assets/html/js/scripts.js
@@ -0,0 +1,61 @@
+/*
+ Author: Ukieweb
+ Template: Washer (Coming Soon)
+ Version: 1.0
+ URL: http://themeforest.net/user/UkieWeb
+ */
+
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+    /*
+     ----------------------------------------------------------------------
+     Preloader
+     ----------------------------------------------------------------------
+     */
+    $(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+
+
+    /*
+     ----------------------------------------------------------------------
+     Nice scroll
+     ----------------------------------------------------------------------
+     */
+    $("html").niceScroll({
+        cursorcolor: '#fff',
+        cursoropacitymin: '0',
+        cursoropacitymax: '1',
+        cursorwidth: '2px',
+        zindex: 999999,
+        horizrailenabled: false,
+        enablekeyboard: false
+    });
+
+
+
+}); // End $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
